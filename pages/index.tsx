c1816b0214ee9fb12c036fa885eea210c9dd136b
leonardo.santos <leonardo.santos@ao3tech.com>
2022-06-25 18:31:51 -0300

    Shuffle button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,27 @@ import type { NextPage } from "next";
 import { useState } from "react";
 import { CanvasComponent } from "../src/components/atoms/Canvas";
 import { Cube, CubeActions } from "../src/components/organisms/Cube";
-
+// l, B L l f UU B l R B u B bbb d L D B d L
 const Home: NextPage = () => {
 	const [action, setAction] = useState<CubeActions>(null);
+
+	function shuffle() {
+		let count = 0;
+		
+		const times = Math.floor(Math.random() * 10) + 14;
+
+		const f = () => {
+			if (count < times) {
+				let actions = ["F" , "B" , "L" , "R" , "U" , "D" , "f" , "b" , "l" , "r" , "u" , "d"];
+				let act = actions[Math.floor(Math.random() * actions.length)] as CubeActions;
+				console.log(act);
+				setAction(act);
+				count++;
+				setTimeout(f, 1000);
+			}
+		}
+		f();
+	}
 
 	return (
 		<div
@@ -27,24 +45,38 @@ const Home: NextPage = () => {
 				>
 					<Cube cubieSize={0.75} action={action} />
 				</CanvasComponent>
-				<input
-					type="text"
-					value={action || ""}
+				<div
 					style={{
-						margin: '2em 0',
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-around",
+						alignItems: "center",
 					}}
-					onChange={(e) => {
-						if (e.target.value !== "") {
-							setAction(
-								e.target.value[
-									e.target.value.length - 1
-								] as CubeActions
-							);
-						} else if (e.target.value === "") {
-							setAction(null);
-						}
-					}}
-				/>
+				>
+					<input
+						type="text"
+						value={action || ""}
+						style={{
+							margin: '2em 0',
+						}}
+						onChange={(e) => {
+							if (e.target.value !== "") {
+								setAction(
+									e.target.value[
+										e.target.value.length - 1
+									] as CubeActions
+								);
+							} else if (e.target.value === "") {
+								setAction(null);
+							}
+						}}
+					/>
+					<button
+						onClick={() => {
+							shuffle();
+						}}
+					>Embaralhar</button>
+				</div>
 
 				<ul>
 					<li>F ou f: Face</li>
