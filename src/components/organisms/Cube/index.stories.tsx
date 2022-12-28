8382b4dd8109c1a55a0c7053300f422dfdce295b
Leonardo Santos <leonardo.santos@iob.com.br>
2022-12-28 09:27:51 -0300

    Fixing Storybook

--- src/components/organisms/Cube/index.stories.tsx
+++ src/components/organisms/Cube/index.stories.tsx
@@ -1,19 +1,27 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Cube } from "./old_index";
+import { Cube, CubeActions } from ".";
 import { CanvasComponent } from "../../atoms/Canvas";
+import { useState } from "react";
+import { useEffect } from "@storybook/addons";
 
 export default {
-    title: "Cube/Cube",
-    component: Cube,
+  title: "Cube/Cube",
+  component: Cube,
 } as ComponentMeta<typeof Cube>;
 
-const Template: ComponentStory<typeof Cube> = (args) => (
-    <CanvasComponent
-        backgroundColor="#eee"
-    >
-        <Cube {...args} />
+const Template: ComponentStory<typeof Cube> = (args) => {
+  const [action, setAction] = useState<CubeActions>(null);
+
+  useEffect(() => {
+    setAction(args.action);
+  }, [args]);
+
+  return (
+    <CanvasComponent backgroundColor="#eee">
+      <Cube {...{...args, action: action, setAction: setAction}} />
     </CanvasComponent>
-);
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {};
